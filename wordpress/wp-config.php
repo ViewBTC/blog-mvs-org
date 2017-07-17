052d4efe716edd3e5c316c5c15e0a5659d91fa58
yangcongtou2016 <2297367632@qq.com>
2017-07-17 18:21:27 +0800

    xh-add717

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -37,6 +37,10 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+/** 修改wordpress根目录为相对路径 */
+define('WP_HOME', '');
+define('WP_SITEURL', '');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
